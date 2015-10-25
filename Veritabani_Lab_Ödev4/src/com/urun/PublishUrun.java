933ef0221ad1f4d2a071fce4e4120e0003da33c1
Ufuk Palavar <ufuk.palavar@bil.omu.edu.tr>
2015-10-25 20:23:07 +0200

    Update PublishUrun.java

--- Veritabani_Lab_Ödev4/src/com/urun/PublishUrun.java
+++ Veritabani_Lab_Ödev4/src/com/urun/PublishUrun.java
@@ -8,7 +8,7 @@ public class PublishUrun {
 
 	public static void main(String[] args) {
 		Endpoint endpoint = Endpoint.publish("http://localhost:8080/urun", new CalUrunServiceImp());
-        System.out.println(endpoint.isPublished());
+                System.out.println(endpoint.isPublished());
 		
 
 	}
